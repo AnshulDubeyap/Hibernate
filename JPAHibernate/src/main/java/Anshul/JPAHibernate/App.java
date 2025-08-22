fb63805033987e1216321e768446dbc38ac72a7e
Anshul Dubey <dubeyanshul2204@gmail.com>
2025-08-22 22:11:52 +0530

    JPA hibernate

---
+++ JPAHibernate/src/main/java/Anshul/JPAHibernate/App.java
@@ -0,0 +1,18 @@
+package Anshul.JPAHibernate;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       
+    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("");
+    	EntityManager em = emf.createEntityManager();
+    	
+    	Alien a = em.find(Alien.class, 1);
+    }
+}
